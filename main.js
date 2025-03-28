b47b5038d121f5fcf7d74664e7df3065830ef2ad
Amit-Patra-04 <k2amitpatra@gmail.com>
2025-03-28 19:11:15 +0530

    Remove Menu functionality

--- main.js
+++ main.js
@@ -19,6 +19,16 @@ if(navClose){
     })
 }
 
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
 /*==================== SWIPER DISCOVER ====================*/
 let swiper = new Swiper(".discover__container", {
     effect: "coverflow",
